13cb39da9341f7a5e573157a03a4a3c874a3fc4b
flvdnc <F@CEPUS>
2016-11-21 19:05:34 +0200

    c7 ex1

---
+++ MvcProjectC7/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectC7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Content("Hello from Index");
+        }
+
+        public ActionResult SomeMethodName()
+        {
+            return Content("Hello from SomeMethodName");
+        }
+    }
+}
